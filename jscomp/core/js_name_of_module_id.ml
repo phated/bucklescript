2c48f1714a7704dd9e659f75bf79a0b37bef6f30
Blaine Bublitz <blaine.bublitz@gmail.com>
2018-06-12 13:57:31 -0700

    Remove ./ for stdlib

--- jscomp/core/js_name_of_module_id.ml
+++ jscomp/core/js_name_of_module_id.ml
@@ -28,9 +28,9 @@
 let string_of_module_id_in_browser (x : Lam_module_ident.t) =  
    match x.kind with
    | External name -> name
-   | Runtime | Ml -> 
-                   "./stdlib/" ^  String.uncapitalize x.id.name ^ ".js"
-let string_of_module_id 
+   | Runtime | Ml ->
+                   "stdlib/" ^  String.uncapitalize x.id.name ^ ".js"
+let string_of_module_id
     ~output_dir:(_:string)
     (_module_system : Js_packages_info.module_system)
     id = string_of_module_id_in_browser id
